bac25f06d3aee9c6594169745a9bc92e4ba47d6b
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-05-16 17:27:31 +0530

    added Header and section

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require("express-sessions");
+
+const app = express();
+
+app.engine(".hbs", exphbs({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// HANDLEBARS
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("landingpage");
+});
+
+app.listen(5000, () => console.log("Server Started"));
