23770aede2c11fc551d8e65e8e007367134c02ff
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-05-17 16:21:11 +0530

    added mongoose connection and login file

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
+require("dotenv").config();
 const express = require("express");
 const exphbs = require("express-handlebars");
-const session = require("express-sessions");
 
-// Import Routes
+const Mongoose = require("mongoose");
 
+// IMPORT ROUTES
 const homeRouter = require("./routes/home");
 const signupRouter = require("./routes/signup");
+const gobackRouter = require("./routes/gobacktohome");
 const app = express();
 
+// MONGO DATABASE CONNECTION
+const { DATABASE_URL } = process.env;
+
+Mongoose.connect(
+  DATABASE_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Mongoose Connected");
+  }
+);
+
+// HANDLEBAR ACTIVATION
 app.engine(".hbs", exphbs({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
-// HANDLEBARS
+// MIDDLEWARE
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 // ROUTES
 app.use("/", homeRouter);
-app.use("/user", signupRouter);
+app.use("/", signupRouter);
+app.use("/", gobackRouter);
 
 app.listen(5001, () => console.log("Server Started"));
