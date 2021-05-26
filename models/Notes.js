04cc243260c8dcc0f7b7e87baf30651f9a1de758
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-05-26 14:15:02 +0530

    [DB] connected todo page with database

--- models/Notes.js
+++ models/Notes.js
@@ -16,7 +16,7 @@ const NotesSchema = mongoose.Schema({
 
   createdAt: {
     type: Date,
-    default: Date.now(),
+    default: Date.now,
   },
 });
 
--- routes/todo.js
+++ routes/todo.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const TodoModel = require("../models/Todo");
 
 router.get("/dashboard/todo", (req, res) => {
   res.render("todo");
+});
+
+router.post("/dashboard/todo", async (req, res) => {
+  const TodoObj = {
+    userId: req.session.user._id,
+    item: req.body.item,
+    count: req.body.count,
+  };
+
+  const newEntry = TodoModel(TodoObj);
+  await newEntry.save();
 });
 
 module.exports = router;
