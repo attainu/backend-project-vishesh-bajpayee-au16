0a95ad7ff8ba28abe1c7c662864281a8b31958c6
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-05-27 14:12:28 +0530

    added test todo page

--- public/script.js
+++ public/script.js
@@ -3,16 +3,30 @@ const addBtn = document.querySelector("#add-btn");
 const todoWrapper = document.querySelector(".todo-items-list");
 const clearAllBtn = document.querySelector("#clear-all");
 let count = 0;
-
 addBtn.addEventListener("click", async function () {
   count += 1;
   let value = createItem.value;
-  todoWrapper.innerHTML += `<div
-  class="todo-item"> <h1 id="item-value">${value}</h1> <div class="icon-wrapper"> <div
-  class="icons"> <i id="popup" class="fas fa-plus"></i> <i id="delete"
-  class="far fa-trash-alt"></i> <i id="check" class="fas fa-check-square"></i>
-  <i id="strike" class="fas fa-strikethrough"></i> <i id="copy" class="far
-  fa-copy"></i> <i id="edit" class="far fa-edit"></i> </div> </div> </div>`;
+  createItem.value = "";
+  todoWrapper.innerHTML += ` <div class="todo-item">
+        <h1 id="item-value">${value}</h1>
+
+        <div class="icon">
+
+          <div id="add">
+
+            <i id="" class="fas fa-check-circle"></i>
+          </div>
+
+          <div class="icon-feature">
+            <i id="delete" class="far fa-trash-alt"></i>
+            <i id="copy" class="far fa-copy"></i>
+            <i id="edit" class="far fa-edit"></i>
+
+          </div>
+        </div>
+
+      </div>
+    </div>`;
   const todoItems = document.querySelectorAll(".todo-item");
   const deleteBtns = document.querySelectorAll("#delete");
   const checkBtns = document.querySelectorAll("#check");
@@ -25,25 +39,30 @@ addBtn.addEventListener("click", async function () {
     const strikeBtn = strikeBtns[index];
     const itemValue = itemValues[index];
     const editBtn = editBtns[index];
-    btn.addEventListener("click", function () {
+    btn.addEventListener("click", async function () {
+      const deletePayload = {
+        item: itemValue[index],
+        count: count,
+      };
+
+      const axiosRequest = await axios.delete(
+        "/dashboard/todo/delete",
+        deletePayload
+      );
       todoItems[index].remove();
     });
     checkBtn.addEventListener("click", function () {
       todoItems[index].style.background = "white";
-      todoItems[index].style.boxShadow = "0px 0px 5px 1px #81b214";
-      //   createItem.color = "black";
+      todoItems[index].style.boxShadow = "0px 0px 5px 1px #81b214"; // createItem.color = "black";
       checkBtn.style.color = "teal";
     });
-
     strikeBtn.addEventListener("click", function () {
       itemValue.style.textDecoration = "line-through";
     });
-
     editBtn.addEventListener("click", function () {
       itemValue.toggleAttribute("contenteditable", true);
     });
   }
-
   const valObj = {
     count: count,
     item: value,
