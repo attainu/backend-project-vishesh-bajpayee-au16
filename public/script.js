cf38a064049ec6e6dc79345af1999d6e899e3542
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-05-31 01:50:31 +0530

    [ANIMATIONS] added few animations to pomodoro page

--- public/script.js
+++ public/script.js
@@ -8,17 +8,30 @@ const hiddenSetTimer = document.querySelector(
 const hiddenDefaultTimer = document.querySelector(
   ".hidden-default-timer-wrapper-position"
 );
+const beginSetBtn = document.getElementById("begin-set");
+const beginDefaultBtn = document.getElementById("begin-default");
 
 const enableDisplay = function (clickBtn, displayDiv) {
   clickBtn.addEventListener("click", function () {
     if (displayDiv.style.display === "none") {
       displayDiv.style.display = "block";
+      displayDiv.style.animation = "opacityup .2s linear ";
     } else {
       displayDiv.style.display = "none";
+      displayDiv.style.animation = "opacityup .2s linear ";
     }
+  });
+};
+const disableDisplay = function (button) {
+  button.addEventListener("click", function () {
+    customDefaultBtn.style.display = "none";
+    hiddenDefaultTimer.style.display = "none";
+    hiddenSetTimer.style.display = "none";
   });
 };
 
 enableDisplay(createBtn, customDefaultBtn);
 enableDisplay(customTimerBtn, hiddenSetTimer);
 enableDisplay(defaultTimerBtn, hiddenDefaultTimer);
+disableDisplay(beginDefaultBtn);
+disableDisplay(beginSetBtn);
