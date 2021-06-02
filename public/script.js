de34f00d35b11dba7fc6e04b11c73e5ebb043447
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-06-02 15:16:35 +0530

    [IMPROVED] pomodoro functionality

--- public/script.js
+++ public/script.js
@@ -39,6 +39,37 @@ const disableDisplay = function (button) {
   });
 };
 
+const setBreakFunc = function (breakVal, delay, index) {
+  let breakValue = breakVal;
+  setTimeout(() => {
+    const breakIntervel = setInterval(() => {
+      breakValue -= 1;
+      breakContainer[index].textContent = `${breakValue}`;
+      if (breakValue == 0) {
+        clearInterval(breakIntervel);
+        breakContainer[index].style.background = "black";
+        breakContainer[index].style.color = "white";
+      }
+    }, 1000);
+  }, delay);
+};
+
+const setTimerFunc = function (timerVal, delay, index) {
+  setTimeout(() => {
+    let timerValue = timerVal;
+
+    const timerIntervel = setInterval(() => {
+      timerValue -= 1;
+      timerContainer[index].setAttribute("class", "animation-class");
+      timeIndicator[index].textContent = `${timerValue}`;
+      if (timerValue == 0) {
+        clearInterval(timerIntervel);
+        timerContainer[index].style.background = "teal";
+      }
+    }, 1000);
+  }, delay);
+};
+
 // EVENT LISTENERS
 beginSetBtn.addEventListener("click", function () {
   const timerInfoList = [];
@@ -53,7 +84,7 @@ beginDefaultBtn.addEventListener("click", function () {
   timerContainer[0].setAttribute("class", "animation-class");
   createBtn.setAttribute("disabled", true);
   createBtn.style.background = "gray";
-  let timeValue = 25;
+  let timeValue = 10;
   const initialIntervel = setInterval(() => {
     timeValue -= 1;
     timeIndicator[0].textContent = `${timeValue}`;
@@ -61,13 +92,13 @@ beginDefaultBtn.addEventListener("click", function () {
       timerContainer[0].style.background = "teal";
       clearInterval(initialIntervel);
     }
-  }, 60000);
-  setBreakFunc(5, 25000, 0);
-  setTimerFunc(25, 30000, 1);
-  setBreakFunc(5, 55000, 1);
-  setTimerFunc(25, 60000, 2);
-  setBreakFunc(5, 85000, 2);
-  setTimerFunc(25, 90000, 3);
+  }, 1000);
+  setBreakFunc(5, 10000, 0);
+  setTimerFunc(10, 15000, 1);
+  setBreakFunc(5, 25000, 1);
+  setTimerFunc(10, 30000, 2);
+  setBreakFunc(5, 45000, 2);
+  setTimerFunc(10, 50000, 3);
 });
 
 enableDisplay(createBtn, customDefaultBtn);
@@ -75,30 +106,3 @@ enableDisplay(customTimerBtn, hiddenSetTimer);
 enableDisplay(defaultTimerBtn, hiddenDefaultTimer);
 disableDisplay(beginDefaultBtn);
 disableDisplay(beginSetBtn);
-
-const setBreakFunc = function (breakVal, delay, index) {
-  let breakValue = breakVal;
-  setTimeout(() => {
-    const breakIntervel = setInterval(() => {
-      breakValue -= 1;
-      breakContainer[index].textContent = `${breakValue}`;
-      if (breakValue == 0) {
-        clearInterval(breakIntervel);
-        breakContainer[index].style.background = "black";
-        breakContainer[index].style.color = "white";
-      }
-    }, 60000);
-  }, delay);
-};
-
-const setTimerFunc = function (timerVal, delay, index) {
-  setTimeout(() => {
-    let timerValue = timerVal;
-
-    setInterval(() => {
-      timerVal -= 1;
-      timerContainer[index].setAttribute("class", "animation-class");
-      timerContainer[index].textContent = `${timerVal}`;
-    }, 60000);
-  }, delay);
-};
