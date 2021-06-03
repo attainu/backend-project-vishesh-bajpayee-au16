6c57fad52b24ebee6fda07017efd407e6cb21059
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-06-03 21:39:19 +0530

    [FUNCTIONALITY] added count updates to database for pomodoro

--- routes/pomodoro.js
+++ routes/pomodoro.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
-
+const PomodoroModel = require("../models/Pomodoro");
 router.get("/dashboard/pomodoro", (req, res) => {
   res.render("pomodoro");
 });
 
+router.post("/dashboard/pomodoro", async (req, res) => {
+  const userObj = req.session.user;
+
+  const pomodoroObjArr = await PomodoroModel.find({
+    userId: userObj._id,
+  }).lean();
+  const pomodoroObj = pomodoroObjArr[0];
+
+  const updatedPomodoro = await PomodoroModel.findOneAndUpdate(
+    { userId: userObj._id },
+    { count: pomodoroObj.count + 1 }
+  );
+  res.json(pomodoroObj);
+});
 module.exports = router;
-23;
