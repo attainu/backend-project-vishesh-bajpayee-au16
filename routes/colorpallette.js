40c099f9a24cc59ddfddc29fa5a07e898fa331cb
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-06-06 11:30:57 +0530

    [FUNCTIONALITY] added delete functionality for color pallette

--- routes/colorpallette.js
+++ routes/colorpallette.js
@@ -8,7 +8,7 @@ router.get("/dashboard/colorpallette", async (req, res) => {
     userId: userObj._id,
   }).lean();
 
-  console.log(userColors);
+  // console.log(userColors);
 
   res.render("colorpallette", {
     pallette: userColors,
@@ -29,12 +29,28 @@ router.post("/dashboard/colorpallette/", async (req, res) => {
         color05: payload.randomColorsArr[4],
       },
     };
-    console.log(payload.randomColorsArr);
+    // console.log(payload.randomColorsArr);
     const newPallette = new ColorpalletteModel(newPalletteObj);
     await newPallette.save();
   } catch (error) {
     res.render("404");
   }
+});
+router.delete("/dashboard/colorpallette/delete", async (req, res) => {
+  const userObj = req.session.user;
+  const deleteObj = req.body;
+  const payloadArr = deleteObj.payload.payload;
+  // console.log(payloadArr);
+  const userPallette = await ColorpalletteModel.find({ userId: userObj._id });
+  await ColorpalletteModel.findOneAndDelete({
+    colors: {
+      color01: payloadArr[0],
+      color02: payloadArr[1],
+      color03: payloadArr[2],
+      color04: payloadArr[3],
+      color05: payloadArr[4],
+    },
+  });
 });
 
 module.exports = router;
